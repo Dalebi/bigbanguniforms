ccbe1c4beead310eaa0a3c6c4b9d17a18d8e2b68
Dalebi <calebi.cbi@gmail.com>
2023-04-20 13:46:20 -0600

    Adding Fontawesome

--- Index.php
+++ Index.php
@@ -12,10 +12,12 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/e91bc97062.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<h1 class="border-bottom border-light pb-2 mb-4 text-white" style="background-image: linear-gradient(90deg, rgba(255,0,0,3), rgba(255,255,255,1))">
-   <a href="/">Big Bang Uniforms</a></h1>    
+<h1 class="border-bottom border-light pb-2 mb-4 text-white " style="background-image: linear-gradient(90deg, rgba(255,0,0,3), rgba(255,255,255,1))">
+   <a href="/"><i class="fa-solid fa-house"></i> Big Bang Uniforms</a></h1>    
 
 <iframe src="Resources/Views/Index.html" frameborder="0" class="container-fluid" style="height: 600px;" ></iframe>
 </body>
